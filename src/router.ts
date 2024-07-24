93e9466053ae2529679263d194f9c5a0367d359e
Otávio Gabriel <107057360+Otavioalv@users.noreply.github.com>
2024-07-24 17:02:55 -0400

    add

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,25 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
+import { Message, messages } from "./socketIO";
 
 export async function routers(fastify: FastifyInstance, options: FastifyPluginOptions) {
     fastify.get('/', async (req: FastifyRequest, res: FastifyReply) => {
         res.status(200).send({message: "success"});
+    });
+
+    fastify.get("/notify", async (req: FastifyRequest, res: FastifyReply) => {
+        res.status(200).send({message: "Ussuarios notificados"});
+        fastify.ready(err => {
+            if(err) throw err;
+
+            const message: Message = {
+                createdAt: new Date(),
+                room: "all",
+                text: "Menssagem do servidor",
+                username: "Servidor"
+            }
+
+            messages.push(message);
+            fastify.io.emit("message", message);
+        });
     });
 }
--- src/socketIO.ts
+++ src/socketIO.ts
@@ -13,7 +13,7 @@ interface RoomUser {
     room: string
 }
 
-interface Message  {
+export interface Message  {
     text: string, 
     username: string, 
     room: string,
@@ -21,7 +21,7 @@ interface Message  {
 }
 
 const users: RoomUser[] = [];
-const messages: Message[] = [];
+export const messages: Message[] = [];
 
 export const socketIO = async (app: FastifyInstance) => {
     console.log("soketIO");
@@ -45,8 +45,8 @@ export const socketIO = async (app: FastifyInstance) => {
                 // coloca usuario em uma sala especifica
                 socket.join(data.room);
                 
-                const userInRoom = users.find(user => user.usermame === data.username && user.room === data.room);
-                
+                const userInRoom = users.find(user => (user.usermame === data.username && user.room === data.room));
+
                 if(userInRoom) {
                     userInRoom.soket_id = socket.id; 
                 }
@@ -69,6 +69,8 @@ export const socketIO = async (app: FastifyInstance) => {
 
                 messages.push(message);
 
+                console.log(messages);
+
                 // utilizamos o APP pra retornar pra todos os usuarios
                 // Enviar para usuario da sala, como base na sala especifica configurada no join
                 app.io.to(data.room).emit("message", message);
@@ -83,7 +85,7 @@ export const socketIO = async (app: FastifyInstance) => {
 }
 
 function getMessagesRoom(room: string) {
-    const messagesRoom = messages.filter(message => message.room === room);
+    const messagesRoom = messages.filter(message => message.room === room || message.room === "all");
 
     return messagesRoom;
 }
